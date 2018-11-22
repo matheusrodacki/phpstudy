fc365907addd003489d19dcd88865383e4a32333
Matheus Rodacki <matheus.rodacki@outlook.com>
2018-11-21 22:12:56 -0200

    Finalizado aula sobre data

---
+++ Date/exemplo-1.php
@@ -0,0 +1,9 @@
+<?php
+
+echo date("d/m/Y H:i:s", 1542842285);
+
+echo "<br>";
+
+echo time();
+
+?>
---
+++ Date/exemplo-3.php
@@ -0,0 +1,9 @@
+<?php
+
+date_default_timezone_set("America/Sao_Paulo");
+
+setlocale(LC_ALL, "pt_BR", "pt_BR.utf-8", "portuguese");
+
+echo ucwords(strftime("%A"))." , dia ".ucwords(strftime("%e"))." de ".ucwords(strftime("%B"))." de ".ucwords(strftime("%Y"));
+
+?>
