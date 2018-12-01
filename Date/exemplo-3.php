682bdde9c808cb366ed5d5cc2f2c30fa3211734d
Matheus Rodacki <matheus.rodacki@outlook.com>
2018-12-01 20:47:08 -0200

    Alterado comando "ucwords", data, ex3

--- Date/exemplo-3.php
+++ Date/exemplo-3.php
@@ -4,6 +4,8 @@ date_default_timezone_set("America/Sao_Paulo");
 
 setlocale(LC_ALL, "pt_BR", "pt_BR.utf-8", "portuguese");
 
-echo ucwords(strftime("%A"))." , dia ".ucwords(strftime("%e"))." de ".ucwords(strftime("%B"))." de ".ucwords(strftime("%Y"));
+//echo ucwords(strftime("%A"))." , dia ".ucwords(strftime("%e"))." de ".ucwords(strftime("%B"))." de ".ucwords(strftime("%Y"));
+
+echo ucwords(strftime('%A, %d de %B de %Y'));
 
 ?>
