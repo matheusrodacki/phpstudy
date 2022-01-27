6f7ea33b181dd89a2ebe2454c7a16ae46d6e36ae
Matheus Rodacki <matheus.rodacki@outlook.com>
2022-01-27 12:02:23 -0300

    Remove mcrypt_encrypt from code.

--- criptografia/exemplo-01.php
+++ criptografia/exemplo-01.php
@@ -1,27 +1,32 @@
 <?php
 
-$data = [
-	"nome" => "Hcode"
-];
+##Esta função está OBSOLETA a partir do PHP 7.1.0 e foi REMOVIDA a partir do PHP 7.2.0. 
+## Depender dessa função é altamente desencorajado.
 
-define('SECRET', pack('a16', 'senha'));
 
-$mcrypt = mcrypt_encrypt(
-	MCRYPT_RIJNDAEL_128,
-	SECRET,
-	json_encode($data),
-	MCRYPT_MODE_ECB
-);
 
-$final = base64_encode($mcrypt);
-
-var_dump($final);
-
-$string = mcrypt_decrypt(
-	MCRYPT_RIJNDAEL_128,
-	SECRET,
-	base64_decode($final),
-	MCRYPT_MODE_ECB
-);
-
-var_dump(json_decode($string, true));
+##$data = [
+##	"nome" => "Hcode"
+##];
+##
+##define('SECRET', pack('a16', 'senha'));
+##
+##$mcrypt = mcrypt_encrypt(
+##	MCRYPT_RIJNDAEL_128,
+##	SECRET,
+##	json_encode($data),
+##	MCRYPT_MODE_ECB
+##);
+##
+##$final = base64_encode($mcrypt);
+##
+##var_dump($final);
+##
+##$string = mcrypt_decrypt(
+##	MCRYPT_RIJNDAEL_128,
+##	SECRET,
+##	base64_decode($final),
+##	MCRYPT_MODE_ECB
+##);
+##
+##var_dump(json_decode($string, true));
